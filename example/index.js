92b11ce468c7a657d7a97411327bb4b33cdad74d
Ionică Bizău <bizauionica@gmail.com>
2016-07-22 12:25:04 +0300

    Examples

--- example/index.js
+++ example/index.js
@@ -2,4 +2,20 @@
 
 const writeFileP = require("../lib");
 
-console.log(writeFileP());
+// Write a text file
+writeFileP(`${__dirname}/foo/bar/output.txt`, "Hello World", (err, data) => {
+    console.log(err || data);
+});
+
+
+// Write a json syncronously
+writeFileP.sync(`${__dirname}/bar/bar/output.json`, {
+    hello: "world"
+});
+// .
+// ├── bar
+// │   └── bar
+// │       └── output.json
+// └── foo
+//     └── bar
+//         └── output.txt
